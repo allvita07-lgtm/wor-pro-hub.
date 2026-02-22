6b0ec2eef7c7b90629ce4f8986f1f484a45641c1
allvita07-lgtm <allvita07@gmail.com>
2026-02-22 03:17:54 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,15 @@
+# data.py
+HEROES = {
+    "Marksman": ["Silas", "Hatssut", "Setram", "Aracha", "Idril", "Calypso", "Nyx", "Razaak", "Maul"],
+    "Mage": ["Khamet", "Vierna", "Boreas", "Ajax", "Morrigan", "Laseer", "Eona"],
+    "Fighter": ["Zilitu", "Valderon", "Lust", "Arrogance", "Salazar", "Wrath", "Volka"],
+    "Tank": ["Olague", "Brokkir", "Torodor", "Regulus", "Baron"],
+    "Healer": ["Elowyn", "Hollow", "Laya", "Sadie", "Vortex"]
+}
+
+STRATEGIES = {
+    "GR1": "Foco: Dano Mágico AoE. Requisito: Anti-cura. MVP: Khamet/Vierna.",
+    "GR2": "Foco: Defesa/Tanque. Requisito: Timing de Volka A1. MVP: Olague/Sadie.",
+    "GR3": "Foco: Alcance/Piercers. Requisito: Limpar laterais. MVP: Silas/Idril."
+}
+
